d2f6aa57670a4601a59b4c2e6bc9233d6ca4038b
Hamzah Ali Abdillah <88259205+AbdillahHamzahAli@users.noreply.github.com>
2023-08-06 13:42:11 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import { toast } from "react-toastify";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBX8pEFi2m_6Gi8nVChwgl2eH8a6wjBuaA",
-  authDomain: "react-books-mark.firebaseapp.com",
-  projectId: "react-books-mark",
-  storageBucket: "react-books-mark.appspot.com",
-  messagingSenderId: "402457353798",
-  appId: "1:402457353798:web:5da5a5c3a510a6d3a37447",
-  databaseURL: "https://react-books-mark-default-rtdb.asia-southeast1.firebasedatabase.app",
+  
 };
 
 // Initialize Firebase
